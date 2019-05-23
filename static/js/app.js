e420e9799d7ba5c673a383a010d608049e702703
thphan12 <47485204+thphan12@users.noreply.github.com>
2019-05-23 18:22:37 -0500

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,41 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+console.log(tableData);
+
+ tableData.forEach(function(sightingReport) {
+  console.log(sightingReport);
+  var row = tbody.append("tr");
+   Object.entries(sightingReport).forEach(function([key, value]) {
+     console.log(key, value);
+     var cell = tbody.append("td");
+     cell.text(value);
+   });
+ });
+
+
+ var submit = d3.select("#submit");
+
+ submit.on("click", function() {
+
+   d3.event.preventDefault();
+ 
+   d3.select(".summary").html("");
+ 
+
+   var inputElement = d3.select("#datetime");
+   var inputValue = inputElement.property("value");
+
+   var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+ 
+   filteredData.forEach((dateData) => {
+     var row = tbody.append("tr");
+     Object.entries(dateData).forEach(([key, value]) => {
+       var cell = tbody.append("td");
+       cell.text(value);
+     });
+   });
+ });
